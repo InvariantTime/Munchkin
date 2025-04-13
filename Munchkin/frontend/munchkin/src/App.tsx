52b54f89715536898ca8347d4b25289ce794f645
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-04-13 15:36:09 +0300

    setup frontend

---
+++ Munchkin/frontend/munchkin/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <div className='bg-red-300'>
+    <h1 className='text-5xl'>
+      Hello munchkin!
+    </h1>
+  </div>
+}
+
+export default App;
---
+++ Munchkin/frontend/munchkin/tailwind.config.js
@@ -0,0 +1,12 @@
+const { url } = require('inspector');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
