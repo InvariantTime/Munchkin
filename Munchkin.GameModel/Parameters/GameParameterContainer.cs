0789d610698f8fdc0ead019c94ff9a079343d8d6
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-04-24 22:39:26 +0300

    parameters container

---
+++ Munchkin.GameModel/Parameters/GameParameterContainer.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Immutable;
+
+namespace Munchkin.GameModel.Parameters;
+
+public class GameParameterContainer : IGameParameterContainer
+{
+    private readonly ImmutableDictionary<IGameParameterKey, IGameParameter> _parameters;
+
+    public IReadOnlyDictionary<IGameParameterKey, IGameParameter> Parameters => _parameters;
+
+    public GameParameterContainer(IDictionary<IGameParameterKey, IGameParameter> parameters)
+    {
+        _parameters = parameters.ToImmutableDictionary();
+    }
+
+    public IGameParameter? GetParameter(IGameParameterKey key)
+    {
+        _parameters.TryGetValue(key, out var value);
+        return value;
+    }
+}
---
+++ Munchkin.GameModel/Parameters/IGameParameterContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace Munchkin.GameModel.Parameters;
+
+public interface IGameParameterContainer
+{
+    IReadOnlyDictionary<IGameParameterKey, IGameParameter> Parameters { get; }
+
+    IGameParameter? GetParameter(IGameParameterKey key);
+}
