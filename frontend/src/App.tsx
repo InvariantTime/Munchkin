16346d913a5394fcf1342ca49510e3259bb7d65b
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-08-30 11:16:23 +0300

    moving project files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#FACD96] h-screen">
+        <Navbar/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () =>
+{
+    return (
+        <div className="top-0 right-0 w-full bg-black/10 h-[50px]">
+            <h1>
+            </h1>
+        </div>
+    );
+}
