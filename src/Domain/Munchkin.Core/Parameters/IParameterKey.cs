a1f73ac70377cb39672f0e34787cb65a4b85028b
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-09-25 17:18:27 +0300

    Start rewriting and improving parameter system

---
+++ src/Domain/Munchkin.Core/Parameters/IParameterKey.cs
@@ -0,0 +1,8 @@
+﻿namespace Munchkin.Core.Parameters;
+
+public interface IParameterKey : IEquatable<IParameterKey>
+{
+    string Id { get; }
+
+    string DisplayName { get; }
+}
---
+++ src/Domain/Munchkin.Core/Parameters/ParameterKeyBuilding.cs
@@ -0,0 +1,17 @@
+﻿using Munchkin.Core.Entities;
+
+namespace Munchkin.Core.Parameters;
+
+public static class ParameterKey
+{
+    public static ParameterKey<T> Create<T>(string id, string display)
+    {
+        return new ParameterKey<T>(id, display);
+    }
+
+    /*public static EntityParameterKey<TEntity, TParam> Create<TEntity, TParam>(string id, string display)
+        where TEntity : class, IEntity
+    {
+        return new EntityParameterKey<TEntity, TParam>(id, display);
+    }*/
+}
