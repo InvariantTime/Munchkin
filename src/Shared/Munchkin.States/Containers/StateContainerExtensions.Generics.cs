b8f5ae66851d154625d84ad7e2e3bbb80b942461
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2026-01-20 19:21:24 +0300

    simple state container extensions

---
+++ src/Shared/Munchkin.States/Containers/StateContainerExtensions.Generics.cs
@@ -0,0 +1,43 @@
+﻿namespace Munchkin.States.Containers;
+
+public static partial class StateContainerExtensions
+{
+    public static IGenericState<T>? GetState<T>(this IStateContainer container, IGenericStateKey<T> key)
+        where T : notnull
+    {
+        return GetState(container, key);
+    }
+
+    public static IGenericState<T>? GetState<T>(this IStateContainer container, IStateKey key)
+        where T : notnull
+    {
+        var state = container.GetState(key);
+
+        if (state is not IGenericState<T> generic)
+            return null;
+
+        return generic;
+    }
+
+    public static IGenericState<T> GetRequiredState<T>(this IStateContainer container, IStateKey key)
+        where T : notnull
+    {
+        var state = GetState<T>(container, key);
+
+        if (state == null)
+            throw new InvalidOperationException($"Unable to get state with key {key.Id}");
+
+        return state;
+    }
+
+    public static IGenericState<T> GetRequiredState<T>(this IStateContainer container, IGenericStateKey<T> key)
+        where T : notnull
+    {
+        var state = GetState(container, key);
+
+        if (state == null)
+            throw new InvalidOperationException($"Unable to get state with key {key.Id}");
+
+        return state;
+    }
+}
---
+++ src/Shared/Munchkin.States/Containers/StateContainerExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Munchkin.States.Containers;
+
+public static partial class StateContainerExtensions
+{
+    public static IState GetRequiredState(this IStateContainer container, IStateKey key)
+    {
+        var state = container.GetState(key);
+
+        if (state == null)
+            throw new InvalidOperationException($"Unable to get state with key {key.Id}");
+
+        return state;
+    }
+}
